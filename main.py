abadbd0c87405e0e74bf7effb268d563622be959
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2022-10-27 21:55:50 +0530

    taking user input for login/register

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from atexit import register
+from random import choices
+import inquirer
+from inquirer.themes import GreenPassion
+
 from atmsimulator.welcome import welcome
+
 
 def main():
     welcome()
-    # login()
+
+    login_or_register = inquirer.list_input(
+        "Login or Register?", choices=["Login", "Register"]
+    )
+
+    if login_or_register == "Login":
+        pass
+        # login()
+    else:
+        pass
+        # register()
+
 
 main()
