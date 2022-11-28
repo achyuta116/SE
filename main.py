51b4ad0f7317c4168857693fd2348a1f0da92a6f
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2022-11-28 11:40:46 +0530

    display server down message when backend is not online

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from atmsimulator.login import login
-
+import requests
+import sys
+from requests.exceptions import ConnectionError
+from simple_chalk import chalk
 
 def main():
+    try:
+        _ = requests.get("http://localhost:3000/")
+    except ConnectionError:
+        print(chalk.red.bold("Bank server is down."))
+        sys.exit(1)
+
     while True:
         login()
 
